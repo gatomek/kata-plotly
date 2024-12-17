22e4291594e99238744cfce7f72f0dc377dc4191
Gatomek <tomasz.galinski@gmail.com>
2024-12-17 19:41:59 +0100

    pse substations

--- pse-ssts/pse_app.py
+++ pse-ssts/pse_app.py
@@ -1,27 +1,52 @@
 import pandas as pd
 import dash_leaflet as dl
+import xml.etree.ElementTree as Xet
 
 from dash import Dash, html
 
+
+def get_param(str):
+    p = str.split(":")
+    return p[1]
+
+
 # Preparing your data for usage *******************************************
 
-ssts = pd.read_csv("ssts.csv")
+rows = []
+xmlparse = Xet.parse('db/sst-report.xml')
+root = xmlparse.getroot()
+for row in root.iter('row'):
+    target = None
+    for cell in row.iter('cell'):
+        target = cell
+    text = target.get('text')
+    parts = text.split()
 
-icon = {
-    "iconUrl": "https://leafletjs.com/examples/custom-icons/leaf-green.png",
-    "shadowUrl": "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-    "iconSize": [38, 95],  # size of the icon
-    "shadowSize": [50, 64],  # size of the shadow
-    "iconAnchor": [
-        22,
-        94,
-    ],  # point of the icon which will correspond to marker's location
-    "shadowAnchor": [4, 62],  # the same for the shadow
-    "popupAnchor": [
-        -3,
-        -76,
-    ],  # point from which the popup should open relative to the iconAnchor
-}
+    name = get_param(parts[0])
+    lat = get_param(parts[2])
+    long = get_param(parts[1])
+    vl_750 = get_param(parts[3])
+    vl_400 = get_param(parts[4])
+    vl_220 = get_param(parts[5])
+    vl_110 = get_param(parts[6])
+    vl_20 = get_param(parts[7])
+    vl_15 = get_param(parts[8])
+
+    rows.append({
+        "name": name,
+        "lat": lat,
+        "long": long,
+        "vl_750": vl_750,
+        "vl_400": vl_400,
+        "vl_220": vl_220,
+        "vl_110": vl_110,
+        "vl_20": vl_20,
+        "vl_15": vl_15}
+    )
+
+cols = ["name", "lat", "long", "vl_750", "vl_400", "vl_220", "vl_110", "vl_20", "vl_15"]
+all_ssts = pd.DataFrame(rows, columns=cols)
+ssts = all_ssts[all_ssts['vl_400'] == '1']
 
 def get_data():
     markers = []
@@ -29,14 +54,14 @@ def get_data():
         markers.append(
             dl.Marker(
                 title=row['name'],
-                position=(row['lat'], row['lng']),
+                position=(row['lat'], row['long']),
                 children=[
-                    dl.Tooltip(row['name']),
-                    dl.Popup(row['name']),
+                    dl.Tooltip( content="<b><h6>" + row['name'] + "</h6></b><br/>LAT: " + row['lat'] + '<br/>LONG: ' + row['long'])
+                    #dl.Popup(row['name']),
                 ],
             )
         )
-    group = dl.LayerGroup( markers, id="layer_group")
+    group = dl.LayerGroup(markers, id="layer_group")
     return group
 
 # App Layout **************************************************************
@@ -53,10 +78,10 @@ app.layout = html.Div(
             className="row",
         ),
         html.Div(
-            dl.Map( [
-                    dl.TileLayer(),
-                    get_data(),
-                    dl.LayersControl(
+            dl.MapContainer([
+                dl.TileLayer(),
+                get_data(),
+                dl.LayersControl(
                     [
                         dl.BaseLayer(
                             dl.TileLayer(),
@@ -72,9 +97,9 @@ app.layout = html.Div(
                             checked=False,
                         ),
                     ])
-                ],
+            ],
                 id="pse-map",
-                center=[52,19],
+                center=[52, 19],
                 zoom=6,
                 style={'height': '90vh'}
             ),
