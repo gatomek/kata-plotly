8f075cb061f5cc74a69e8b1345ddca684416f99e
Gatomek <tomasz.galinski@gmail.com>
2024-12-20 23:21:41 +0100

    pse substations

--- pse-ssts/pse_app.py
+++ pse-ssts/pse_app.py
@@ -1,70 +1,71 @@
 import pandas as pd
 import dash_leaflet as dl
+import dash_leaflet.express as dlx
 import xml.etree.ElementTree as Xet
 
 from dash import Dash, html
 
+# lib ***
 
-def get_param(str):
-    p = str.split(":")
+def get_param(txt):
+    p = txt.split(":")
     return p[1]
 
+def load_data(filePath):
+    rows = []
+    xmlparse = Xet.parse(filePath)
+    root = xmlparse.getroot()
+    for row in root.iter('row'):
+        target = None
+        for cell in row.iter('cell'):
+            target = cell
+        text = target.get('text')
+        parts = text.split()
 
-# Preparing your data for usage *******************************************
+        name = get_param(parts[0])
+        lat = get_param(parts[2])
+        lon = get_param(parts[1])
+        vl_750 = get_param(parts[3])
+        vl_400 = get_param(parts[4])
+        vl_220 = get_param(parts[5])
+        vl_110 = get_param(parts[6])
+        vl_20 = get_param(parts[7])
+        vl_15 = get_param(parts[8])
 
-rows = []
-xmlparse = Xet.parse('db/sst-report.xml')
-root = xmlparse.getroot()
-for row in root.iter('row'):
-    target = None
-    for cell in row.iter('cell'):
-        target = cell
-    text = target.get('text')
-    parts = text.split()
+        rows.append({
+            "name": name,
+            "lat": lat,
+            "lon": lon,
+            "vl_750": vl_750,
+            "vl_400": vl_400,
+            "vl_220": vl_220,
+            "vl_110": vl_110,
+            "vl_20": vl_20,
+            "vl_15": vl_15}
+        )
 
-    name = get_param(parts[0])
-    lat = get_param(parts[2])
-    long = get_param(parts[1])
-    vl_750 = get_param(parts[3])
-    vl_400 = get_param(parts[4])
-    vl_220 = get_param(parts[5])
-    vl_110 = get_param(parts[6])
-    vl_20 = get_param(parts[7])
-    vl_15 = get_param(parts[8])
+    cols = ["name", "lat", "lon", "vl_750", "vl_400", "vl_220", "vl_110", "vl_20", "vl_15"]
+    return pd.DataFrame(rows, columns=cols)
 
-    rows.append({
-        "name": name,
-        "lat": lat,
-        "long": long,
-        "vl_750": vl_750,
-        "vl_400": vl_400,
-        "vl_220": vl_220,
-        "vl_110": vl_110,
-        "vl_20": vl_20,
-        "vl_15": vl_15}
-    )
-
-cols = ["name", "lat", "long", "vl_750", "vl_400", "vl_220", "vl_110", "vl_20", "vl_15"]
-all_ssts = pd.DataFrame(rows, columns=cols)
-ssts = all_ssts[all_ssts['vl_400'] == '1']
-
-def get_data():
+def make_markers(ssts):
     markers = []
     for index, row in ssts.iterrows():
         markers.append(
-            dl.Marker(
-                title=row['name'],
-                position=(row['lat'], row['long']),
-                children=[
-                    dl.Tooltip( content="<b><h6>" + row['name'] + "</h6></b><br/>LAT: " + row['lat'] + '<br/>LONG: ' + row['long'])
-                    #dl.Popup(row['name']),
-                ],
-            )
+            dict(name=row["name"], lat=row["lat"], lon=row["lon"])
         )
-    group = dl.LayerGroup(markers, id="layer_group")
-    return group
+    return markers
 
-# App Layout **************************************************************
+# Data processing ***
+
+all_ssts = load_data('db/sst-report.xml')
+ssts = all_ssts[all_ssts['vl_110'] == '1']
+markers = make_markers(ssts)
+
+geojson = dlx.dicts_to_geojson(
+    [{**c, **dict(tooltip="<b><h6>" + c['name'] + "</h6></b><br/>LAT: " + c['lat'] + '<br/>LON: ' + c['lon'])}
+     for c in markers])
+
+# App Layout ***
 
 stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = Dash(__name__, external_stylesheets=stylesheets)
@@ -72,15 +73,9 @@ app = Dash(__name__, external_stylesheets=stylesheets)
 app.layout = html.Div(
     [
         html.Div(
-            html.H1(
-                "PSE Stacje", style={"textAlign": "center"}
-            ),
-            className="row",
-        ),
-        html.Div(
             dl.MapContainer([
                 dl.TileLayer(),
-                get_data(),
+                dl.GeoJSON(data=geojson, cluster=True),
                 dl.LayersControl(
                     [
                         dl.BaseLayer(
@@ -101,7 +96,7 @@ app.layout = html.Div(
                 id="pse-map",
                 center=[52, 19],
                 zoom=6,
-                style={'height': '90vh'}
+                style={'height': '100vh'}
             ),
             className="row "
         )
