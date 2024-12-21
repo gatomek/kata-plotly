1f89385dd03dfb35fe5d92ff656e8c3c196907d3
Gatomek <tomasz.galinski@gmail.com>
2024-12-21 23:05:22 +0100

    pse substations

--- pse-ssts/pse_app.py
+++ pse-ssts/pse_app.py
@@ -5,11 +5,13 @@ import xml.etree.ElementTree as Xet
 
 from dash import Dash, html
 
+
 # lib ***
 
 def get_param(txt):
     p = txt.split(":")
     return p[1]
+
 
 def load_data(filePath):
     rows = []
@@ -20,30 +22,37 @@ def load_data(filePath):
         vl_splits = vls.split(',')
         vl_array = []
         for vl in vl_splits:
-            if( vl != ''):
-                vl_array.append( float(vl))
+            if (vl != ''):
+                vl_array.append(float(vl))
         vl_array.sort(reverse=True)
-        vl_str = list( map( str, vl_array))
+        vl_str = []
+        for vl in vl_array:
+            vlf = float(vl)
+            vli = int(vl)
+            svl = str(vli) if vlf == vli else str(vlf)
+            vl_str.append(svl)
         rows.append({
-                "desc": row.get('desc'),
-                "geo": row.get('geo'),
-                "name": row.get('name'),
-                "lat": row.get('lat'),
-                "lon": row.get('lon'),
-                "path": row.get('path'),
-                "vls": ', '.join( vl_str),
-                "vl_750": True if (750 in vl_array) else False,
-                "vl_400": True if (400 in vl_array) else False,
-                "vl_220": True if (220 in vl_array) else False,
-                "vl_110": True if (110 in vl_array) else False,
-                "vl_20": True if (20 in vl_array) else False,
-                "vl_15": True if (15 in vl_array) else False,
-                "vl_10": True if (10 in vl_array) else False
-            }
+            "desc": row.get('desc'),
+            "geo": row.get('geo'),
+            "name": row.get('name'),
+            "lat": row.get('lat'),
+            "lon": row.get('lon'),
+            "path": row.get('path'),
+            "vls": ', '.join(vl_str),
+            "vl_750": True if (750 in vl_array) else False,
+            "vl_400": True if (400 in vl_array) else False,
+            "vl_220": True if (220 in vl_array) else False,
+            "vl_110": True if (110 in vl_array) else False,
+            "vl_20": True if (20 in vl_array) else False,
+            "vl_15": True if (15 in vl_array) else False,
+            "vl_10": True if (10 in vl_array) else False
+        }
         )
 
-    cols = ["desc", "geo", "name", "lat", "lon", "path", "vls", "vl_750", "vl_400", "vl_220", "vl_110", "vl_20", "vl_15", "vl_10"]
+    cols = ["desc", "geo", "name", "lat", "lon", "path", "vls", "vl_750", "vl_400", "vl_220", "vl_110", "vl_20",
+            "vl_15", "vl_10"]
     return pd.DataFrame(rows, columns=cols)
+
 
 def make_markers(ssts):
     markers = []
@@ -53,10 +62,11 @@ def make_markers(ssts):
         )
     return markers
 
+
 # Data processing ***
 
 all_ssts = load_data('db/pse-geo-ssts.xml')
-ssts = all_ssts[ all_ssts['vl_220'] == True]
+ssts = all_ssts[all_ssts['vl_220'] == True]
 markers = make_markers(ssts)
 geojson = dlx.dicts_to_geojson(
     [{**c, **dict(tooltip="<h6><b>" + c['desc'] + "</b><br/>" + c['vls'] + "</h6>")}
