8ee8bf99d983858667b5974a48a2839ef864ac57
Gatomek <tomasz.galinski@gmail.com>
2024-12-22 10:21:36 +0100

    pse substations

--- pse-ssts/pse_app.py
+++ pse-ssts/pse_app.py
@@ -1,9 +1,11 @@
+from pydoc import classname
+
 import pandas as pd
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
 import xml.etree.ElementTree as Xet
 
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output
 
 
 # lib ***
@@ -22,15 +24,15 @@ def load_data(filePath):
         vl_splits = vls.split(',')
         vl_array = []
         for vl in vl_splits:
-            if (vl != ''):
+            if vl != '':
                 vl_array.append(float(vl))
         vl_array.sort(reverse=True)
         vl_str = []
         for vl in vl_array:
             vlf = float(vl)
             vli = int(vl)
-            svl = str(vli) if vlf == vli else str(vlf)
-            vl_str.append(svl)
+            svl = vli if vlf == vli else vlf
+            vl_str.append(str(svl))
         rows.append({
             "desc": row.get('desc'),
             "geo": row.get('geo'),
@@ -66,13 +68,26 @@ def make_markers(ssts):
 # Data processing ***
 
 all_ssts = load_data('db/pse-geo-ssts.xml')
-ssts = all_ssts[all_ssts['vl_220'] == True]
-markers = make_markers(ssts)
-geojson = dlx.dicts_to_geojson(
-    [{**c, **dict(tooltip="<h6><b>" + c['desc'] + "</b><br/>" + c['vls'] + "</h6>")}
-     for c in markers])
 
 # App Layout ***
+
+tileLayer = dl.TileLayer()
+layerControl = dl.LayersControl(
+    [
+        dl.BaseLayer(
+            tileLayer,
+            name="OpenStreetMaps",
+            checked=True,
+        ),
+        dl.BaseLayer(
+            dl.TileLayer(
+                url="https://www.ign.es/wmts/mapa-raster?request=getTile&layer=MTN&TileMatrixSet=GoogleMapsCompatible&TileMatrix={z}&TileCol={x}&TileRow={y}&format=image/jpeg",
+                attribution="IGN",
+            ),
+            name="IGN",
+            checked=False,
+        ),
+    ])
 
 stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = Dash(__name__, external_stylesheets=stylesheets)
@@ -80,35 +95,56 @@ app = Dash(__name__, external_stylesheets=stylesheets)
 app.layout = html.Div(
     [
         html.Div(
-            dl.MapContainer([
-                dl.TileLayer(),
-                dl.GeoJSON(data=geojson, cluster=True),
-                dl.LayersControl(
-                    [
-                        dl.BaseLayer(
-                            dl.TileLayer(),
-                            name="OpenStreetMaps",
-                            checked=True,
-                        ),
-                        dl.BaseLayer(
-                            dl.TileLayer(
-                                url="https://www.ign.es/wmts/mapa-raster?request=getTile&layer=MTN&TileMatrixSet=GoogleMapsCompatible&TileMatrix={z}&TileCol={x}&TileRow={y}&format=image/jpeg",
-                                attribution="IGN",
-                            ),
-                            name="IGN",
-                            checked=False,
-                        ),
-                    ])
+            [
+                dl.MapContainer(
+                    children=[],
+                    id="pse-map",
+                    center=[52, 19],
+                    zoom=6,
+                    style={'height': '90vh'},
+                ),
             ],
-                id="pse-map",
-                center=[52, 19],
-                zoom=6,
-                style={'height': '98vh'}
+            className="row"
+        ),
+        html.Div(
+            dcc.Checklist(
+                id="vls_checklist",
+                options=[
+                    {'label': [html.Span('750 kV', style={'margin': '10px'})], 'value': 'vl_750'},
+                    {'label': [html.Span('400 kV', style={'margin': '10px'})], 'value': 'vl_400'},
+                    {'label': [html.Span('220 kV', style={'margin': '10px'})], 'value': 'vl_220'},
+                    {'label': [html.Span('110 kV', style={'margin': '10px'})], 'value': 'vl_110'},
+                ],
+                value=[],
+                inline=True,
+                style={'textAlign': 'center'}
             ),
-            className="row "
-        )
+            className="row"
+        ),
     ]
 )
+
+# Callbacks ***
+@app.callback(
+    Output("pse-map", "children"),
+    [Input("vls_checklist", "value")]
+)
+def update_graph(chosen_value):
+    final_filter = pd.Series(False, range(all_ssts.size))
+    for cv in chosen_value:
+        final_filter |= all_ssts[cv] == True
+
+    ssts = all_ssts.loc[final_filter]
+    markers = make_markers(ssts)
+    geojson = dlx.dicts_to_geojson(
+        [{**c, **dict(tooltip="<h6><b>" + c['desc'] + "</b><br/>" + c['vls'] + "</h6>")}
+         for c in markers])
+
+    return [
+        dl.GeoJSON(data=geojson, cluster=True),
+        layerControl
+    ]
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
